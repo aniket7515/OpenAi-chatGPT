73bfcae56a60ef480f18c05b36e635e856144ca6
Aniket <aniketdhokane2071@gmail.com>
2023-03-04 16:52:37 +0530

    user controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,61 @@
-exports.registerController= async()=>{}
-exports.loginController=async()=>{}
-exports.logoutController= async()=>{}
+const userModel= require("../models/userModel")
+const errorResponse = require("../utils/errorResponse")
+
+// JWT TOKEN
+exports.sendToken=(user,statusCode,res)=>{
+    const token= user.getSignedToken(res)
+    res.status(statusCode).json({
+        success:true,
+        token,
+    })
+}
+
+// REGISTER
+
+exports.registerController= async(req,res,next)=>{
+    try {
+        const {username, email , password}= req.body
+        // existing user
+        const existingEmail= await userModel.findOne({email})
+        if(existingEmail){
+            return next(new errorResponse)
+        }
+        const user= await userModel.create({username, email , password})
+        this.sendToken(user,201,res)
+    } catch (error) {
+        console.log(error);
+        next(error)
+    }
+}
+
+// LOGIN
+exports.loginController=async()=>{
+    try {
+        const {email, password}= req.body
+        if(!email || !password){
+            return next(new errorResponse('Please provide email or password'))
+        }
+        const user= await userModel.findOne({email})
+        if(!user){
+            return next(new errorResponse('Invalid Credential',401))
+        }
+        const isMatch= await userModel.matchPassword(password)
+        if(!isMatch){
+            return next(new errorHandler('Invalid Credential',401))
+        }
+        this.sendToken(user, 200, res)
+    } catch (error) {
+        console.log(error);
+        next(error)
+    }
+}
+
+// LOGOUT
+exports.logoutController= async(req,res)=>{
+    res.clearCookie('refreshToken')
+    return res.status(200).json({
+        success:true,
+        message:'Logout succesfully'
+    })
+
+}
--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const bodyParser= require('body-parser')
 const colors= require('colors')
 const dotenv= require('dotenv')
 const connectDB = require('./config/db')
-
+const errorHandler = require('./middlewares/errorMiddleware.js')
 // routes path
 const authRoutes= require('./routes/authRoutes')
+
 
 // dotenv
 dotenv.config()
@@ -23,9 +24,16 @@ app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(morgan('dev'))
+app.use(errorHandler)
 
 
 const PORT= process.env.PORT || 8080
+
+
+// api routes
+app.use("/api/v1/auth", authRoutes)
+
+
 // listen server
 app.listen(PORT,()=>{
     console.log(`Server running at port ${process.env.DEV_MODE} `.bgCyan.white);
